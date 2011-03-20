a0ae4fe42478d216965bf25fb9d63c4316d6af02
Luis Ibanez <luis.ibanez@kitware.com>
2011-03-19 22:33:48 -0400

    ENH: Start adding Mahalanobis filter.

---
+++ Source/itkMahalanobisDistanceImageFilter.h
@@ -0,0 +1,146 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkMahalanobisDistanceImageFilter_h
+#define __itkMahalanobisDistanceImageFilter_h
+
+#include "itkUnaryFunctorImageFilter.h"
+#include "itkConceptChecking.h"
+#include "itkSimpleDataObjectDecorator.h"
+
+namespace itk
+{
+/** \class MahalanobisDistanceImageFilter
+ *
+ * \brief Compute the Mahalanobis distance at every pixel of an input image.
+ *
+ */
+namespace Functor
+{
+template< class TInput, class TOutput >
+class MahalanobisDistance
+{
+public:
+  typedef Statistics::MahalanobisDistanceMetric< TInput >   DistanceMetricType;
+  typedef DistanceMetricType::MeanVectorType                MeanVectorType;
+  typedef DistanceMetricType::CovarianceMatrixType          CovarianceMatrixType;
+
+  MahalanobisDistance()
+  {
+    m_DistanceMetric = DistanceMetricType::New();
+  }
+
+  ~MahalanobisDistance() {}
+
+  void SetMean(const MeanVectorType & mean)
+  {
+    m_Mean = mean;
+    m_DistanceMetric->SetMean( mean );
+  }
+
+  void SetCovariance(const CovarianceMatrixType & covariance)
+  {
+    m_Covariance = covariance;
+    m_DistanceMetric->SetCovariance( covariance );
+  }
+
+  bool operator!=(const MahalanobisDistance & other) const
+  {
+    if ( m_Mean != other.m_Mean ||
+         m_Covariance != other.m_Covariance )
+      {
+      return true;
+      }
+    return false;
+  }
+
+  bool operator==(const MahalanobisDistance & other) const
+  {
+    return !( *this != other );
+  }
+
+  inline TOutput operator()(const TInput & A) const
+  {
+    return m_DistanceMetric->Evaluate( A );
+  }
+
+private:
+  DistanceMetricType::Pointer   m_DistanceMetric;
+  CovarianceMatrixType          m_Covariance;
+  MeanVectorType                m_Mean;
+};
+}
+
+template< class TInputImage, class TOutputImage >
+class ITK_EXPORT MahalanobisDistanceImageFilter:
+  public
+  UnaryFunctorImageFilter< TInputImage, TOutputImage,
+                           Functor::MahalanobisDistance<
+                             typename TInputImage::PixelType,
+                             typename TOutputImage::PixelType > >
+{
+public:
+  /** Standard class typedefs. */
+  typedef MahalanobisDistanceImageFilter Self;
+  typedef UnaryFunctorImageFilter< TInputImage, TOutputImage,
+                                   Functor::MahalanobisDistance<
+                                     typename TInputImage::PixelType,
+                                     typename TOutputImage::PixelType >
+                                   >                                   Superclass;
+  typedef SmartPointer< Self >       Pointer;
+  typedef SmartPointer< const Self > ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(MahalanobisDistanceImageFilter, UnaryFunctorImageFilter);
+
+  typedef Functor::MahalanobisDistance<
+    typename TInputImage::PixelType,
+    typename TOutputImage::PixelType >              DistanceMetricType;
+
+  typedef DistanceMetricType::MeanVectorType        MeanVectorType;
+  typedef DistanceMetricType::CovarianceMatrixType  CovarianceMatrixType;
+
+#ifdef ITK_USE_CONCEPT_CHECKING
+  /** Begin concept checking */
+  /** End concept checking */
+#endif
+protected:
+  MahalanobisDistanceImageFilter();
+  virtual ~MahalanobisDistanceImageFilter() {}
+  void PrintSelf(std::ostream & os, Indent indent) const;
+
+  /** This method is used to set the state of the filter before
+   * multi-threading. */
+  virtual void BeforeThreadedGenerateData();
+
+private:
+  MahalanobisDistanceImageFilter(const Self &); //purposely not implemented
+  void operator=(const Self &);             //purposely not implemented
+
+  CovarianceMatrixType    m_Covariance;
+  MeanVectorType          m_Mean;
+};
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkMahalanobisDistanceImageFilter.txx"
+#endif
+
+#endif
---
+++ Source/itkMahalanobisDistanceImageFilter.txx
@@ -0,0 +1,54 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkMahalanobisDistanceImageFilter_txx
+#define __itkMahalanobisDistanceImageFilter_txx
+
+#include "itkMahalanobisDistanceImageFilter.h"
+
+namespace itk
+{
+
+template< class TInputImage, class TOutputImage >
+MahalanobisDistanceImageFilter< TInputImage, TOutputImage >
+::MahalanobisDistanceImageFilter()
+{
+}
+
+template< class TInputImage, class TOutputImage >
+void
+MahalanobisDistanceImageFilter< TInputImage, TOutputImage >
+::PrintSelf(std::ostream & os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+
+  os << indent << "Mean: " << m_Mean << std::endl;
+  os << indent << "Covariance: " << m_Covariance << std::endl;
+}
+
+template< class TInputImage, class TOutputImage >
+void
+MahalanobisDistanceImageFilter< TInputImage, TOutputImage >
+::BeforeThreadedGenerateData()
+{
+  // Setup up the functor
+  this->GetFunctor().SetMean( m_Mean );
+  this->GetFunctor().SetCovariance( m_Covariance );
+}
+} // end namespace itk
+
+#endif
