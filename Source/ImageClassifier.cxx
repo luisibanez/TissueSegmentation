328bb9a2e0cb5d13f0502961e1d8d7104b00bd04
Luis Ibanez <luis.ibanez@kitware.com>
2011-05-04 19:49:49 -0400

    ENH: Improved coloring and added mask extraction.

--- Source/ImageClassifier.cxx
+++ Source/ImageClassifier.cxx
@@ -100,7 +100,7 @@ int main(int argc, char * argv [] )
 
   for( unsigned int j = 0; j < numberOfClasses; j++ )
     {
-    classLabelVector.push_back( 100 + j * 50 );
+    classLabelVector.push_back( j );
 
     GaussianMembershipFunctionType::Pointer membershipFunction = GaussianMembershipFunctionType::New();
 
